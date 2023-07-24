c9fa4f0a09c2893ccadc70383710db8f6e5cd26c
Abraham <kurerelagat01@gmail.com>
2023-07-24 11:57:41 +0300

    My Portfolio websites

---
+++ scrypt.js
@@ -0,0 +1,8 @@
+// Get the OK button
+var okButton = document.getElementById("okButton");
+
+// Event listener for the OK button
+okButton.addEventListener("click", function () {
+  // Replace 'https://example.com' with your desired link
+  window.location.href = "https://github.com/AbrahamKiplagat";
+});
